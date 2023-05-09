3a0e2df2f7b5fa667e11e553276b37d67739aced
Nat Lee <natlee.work@gmail.com>
2023-05-09 11:27:41 +0800

    [update] update prompt

--- src/custom_scripts/custom-services.d/autossh.sh
+++ src/custom_scripts/custom-services.d/autossh.sh
@@ -3,11 +3,11 @@
 export $(cat /config/.env)
 
 echo "-------- Start AutoSSH"
-echo "Reverse server Host is $REVERSE_SERVER_HOST"
-echo "Reverse server user is $REVERSE_SERVER_USER_NAME"
-echo "Reverse server SSH port is $REVERSE_SERVER_SSH_PORT"
-echo "Reverse tunnel port is using $REVERSE_TUNNEL_SSH_PORT"
-echo "Reverse using USER is $(whoami)"
+echo "[Reverse Server] Host is $REVERSE_SERVER_HOST"
+echo "[Reverse Server] User is $REVERSE_SERVER_USER_NAME"
+echo "[Reverse Server] SSH port is $REVERSE_SERVER_SSH_PORT"
+echo "Use port [$REVERSE_TUNNEL_SSH_PORT] for our reverse tunnel"
+echo "Client container is using USER -> $(whoami)"
 
 /usr/bin/autossh \
     -M 6767 \
